c0421ead34046fb230dfa90bff8cec9f7d787c57
Andrey Radionov <andyomsk@gmail.com>
2017-12-30 18:03:21 +0300

    S12.04-Ex-Kotlin UI converted

--- S12.04-Exercise-ResourceQualifiers/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.kt
+++ S12.04-Exercise-ResourceQualifiers/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.kt
@@ -18,6 +18,7 @@ package com.example.android.sunshine.sync
 import android.content.Context
 import android.content.Intent
 import com.example.android.sunshine.data.WeatherContract
+import com.example.android.sunshine.utilities.TriggerConfigurator
 import com.firebase.jobdispatcher.*
 import java.util.concurrent.TimeUnit
 
@@ -45,37 +46,30 @@ private fun scheduleFirebaseJobDispatcherSync(context: Context) {
     val dispatcher = FirebaseJobDispatcher(driver)
 
     /* Create the Job to periodically sync Sunshine */
-    val syncSunshineJob = dispatcher.newJobBuilder()
-            /* The Service that will be used to sync Sunshine's data */
-            .setService(SunshineFirebaseJobService::class.java)
-            /* Set the UNIQUE tag used to identify this Job */
-            .setTag(SUNSHINE_SYNC_TAG)
-            /*
-             * Network constraints on which this Job should run. We choose to run on any
-             * network, but you can also choose to run only on un-metered networks or when the
-             * device is charging. It might be a good idea to include a preference for this,
-             * as some users may not want to download any data on their mobile plan. ($$$)
-             */
-            .setConstraints(Constraint.ON_ANY_NETWORK)
-            /*
-             * setLifetime sets how long this job should persist. The options are to keep the
-             * Job "forever" or to have it die the next time the device boots up.
-             */
-            .setLifetime(Lifetime.FOREVER)
-            /*
-             * We want Sunshine's weather data to stay up to date, so we tell this Job to recur.
-             */
-            .setRecurring(true)
-            /*
-             * We want the weather data to be synced every 3 to 4 hours. The first argument for
-             * Trigger's static executionWindow method is the start of the time frame when the
-             * sync should be performed. The second argument is the latest point in time at
-             * which the data should be synced. Please note that this end time is not
-             * guaranteed, but is more of a guideline for FirebaseJobDispatcher to go off of.
-             */
-            .setTrigger(Trigger.executionWindow(
-                    SYNC_INTERVAL_SECONDS,
-                    SYNC_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS))
+    val syncSunshineJob = TriggerConfigurator.executionWindow(
+            dispatcher.newJobBuilder()
+                    /* The Service that will be used to sync Sunshine's data */
+                    .setService(SunshineFirebaseJobService::class.java)
+                    /* Set the UNIQUE tag used to identify this Job */
+                    .setTag(SUNSHINE_SYNC_TAG)
+                    /*
+                     * Network constraints on which this Job should run. We choose to run on any
+                     * network, but you can also choose to run only on un-metered networks or when the
+                     * device is charging. It might be a good idea to include a preference for this,
+                     * as some users may not want to download any data on their mobile plan. ($$$)
+                     */
+                    .setConstraints(Constraint.ON_ANY_NETWORK)
+                    /*
+                     * setLifetime sets how long this job should persist. The options are to keep the
+                     * Job "forever" or to have it die the next time the device boots up.
+                     */
+                    .setLifetime(Lifetime.FOREVER)
+                    /*
+                     * We want Sunshine's weather data to stay up to date, so we tell this Job to recur.
+                     */
+                    .setRecurring(true),
+            SYNC_INTERVAL_SECONDS,
+            SYNC_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS)
             /*
              * If a Job with the tag with provided already exists, this new job will replace
              * the old one.
@@ -168,7 +162,7 @@ fun initialize(context: Context) {
  *
  * @param context The Context used to start the IntentService for the sync.
  */
-private fun startImmediateSync(context: Context) {
+fun startImmediateSync(context: Context) {
     val intentToSyncImmediately = Intent(context, SunshineSyncIntentService::class.java)
     context.startService(intentToSyncImmediately)
 }
